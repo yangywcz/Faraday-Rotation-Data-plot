1aae9df685ff0f91307118eb3978f4098fcc613e
Yang Weihao <32047590+yangywcz@users.noreply.github.com>
2025-03-13 15:51:51 +0800

    Update fr_gui arcsin.py

--- fr_gui arcsin.py
+++ fr_gui arcsin.py
@@ -1,36 +1,26 @@
 import os
-import qtmodern.styles
-import qtmodern.windows
 import numpy as np
 import pandas as pd
-
-import PySide6
+from PySide6.QtGui import QIcon, QFont
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow,QMessageBox,QSplashScreen
+from configparser import ConfigParser
 import pyqtgraph as pg
-from pyqtgraph import PlotWidget
-from pyqtgraph.Qt import QtCore, QtGui
-# from PySide6.QtGui import QIcon, QFont, QPixmap
-# from PySide6.QtCore import QThread, Signal, QTimer, QDate, QTime, QDateTime, Qt, QTranslator
-# from PySide6.QtUiTools import QUiLoader
-# from PySide6.QtWidgets import QApplication, QFileDialog, QMessageBox, QSplashScreen,QProgressBar, QGraphicsView
-# from PySide6.QtWidgets import QFileDialog, QMessageBox
-
-
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import time
 plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 
-WindowTemplate, TemplateBaseClass = pg.Qt.loadUiType("fr.ui")
+class Stats():
 
-class Stats(TemplateBaseClass):
-
-    def __init__(self):
-        TemplateBaseClass.__init__(self)
-        self.ui = WindowTemplate()
-        self.ui.setupUi(self)
-
-        self.file =['','']
-        from configparser import ConfigParser
+    def __init__(self): 
+        loader = QUiLoader()
+        loader.registerCustomWidget(pg.PlotWidget)
+        self.ui = loader.load("fr.ui")
+        
         self.config = ConfigParser()
         if os.path.exists('config.ini'):
             self.config.read('config.ini', encoding='UTF-8')
@@ -38,7 +28,7 @@ class Stats(TemplateBaseClass):
             self.config['Default'] = {'End_line': 30, 'offset_degree': 2.0, 'thickness': 100.0, 'init_dir':f'{os.getcwd()}'}
             with open('config.ini', 'w', encoding='utf-8') as file:
                 self.config.write(file)
-            
+                
         self.dir=self.config['Default']['init_dir']
         self.ui.spinBox_end.setValue(int(self.config['Default']['End_line']))
         self.ui.deg.setValue(float(self.config['Default']['offset_degree']))
@@ -60,19 +50,19 @@ class Stats(TemplateBaseClass):
         
 
     def browser(self):
-        self.file = QtGui.QFileDialog.getOpenFileName(caption=f"选择法拉第测试文件",directory=self.dir,filter="Excel表格文件 (*.xls *.xlsx)")
-        if self.file[0] != '':
+        self.file = QFileDialog.getOpenFileName(caption=f"选择法拉第测试文件",dir=self.dir,filter="Excel表格文件 (*.xls *.xlsx)")
+        if self.file:
             self.dir = os.path.dirname(self.file[0])
             self.config['Default']['init_dir']=self.dir
             with open('config.ini', 'w', encoding='utf-8') as file:
                 self.config.write(file)
             self.ui.lineEdit.setText(os.path.basename(self.file[0]))
-            QtGui.QApplication.processEvents()
+            QApplication.processEvents()
             try:
                 df = pd.read_excel(self.file[0],usecols=['磁场(G)','电流(A)'])
             except:
-                msgBox = QtGui.QMessageBox(QtGui.QMessageBox.Critical,'错误','文件读取失败!')
-                msgBox.exec_()
+                msgBox = QMessageBox(QMessageBox.Critical,'错误','文件读取失败!')
+                msgBox.exec()
                 self.ui.statusbar.showMessage('文件读取失败!')   
                 self.ui.plot1.setEnabled(False)
                 self.ui.plot2.setEnabled(False)
@@ -96,7 +86,8 @@ class Stats(TemplateBaseClass):
             self.ui.plot2.setEnabled(False)
             self.ui.plot3.setEnabled(False)
             self.ui.plot4.setEnabled(False)
-            
+        
+    
 
     def plot1(self):
         self.ui.qtplot.setLabel("left", "法拉第旋角 (deg)")
@@ -115,24 +106,33 @@ class Stats(TemplateBaseClass):
         co = self.ui.spinBox_start.value()
         ci = self.ui.spinBox_end.value()
         if co == ci:
-            msgBox = QtGui.QMessageBox(QtGui.QMessageBox.Critical,'错误','起始行数和结束行数必须不同!')
-            msgBox.exec_()
+            msgBox = QMessageBox(QMessageBox.Critical,'错误','起始行数和结束行数必须不同!')
+            msgBox.exec()
             self.ui.statusbar.showMessage('起始行数和结束行数必须不同!')
         elif co>len(df.index) or ci>len(df.index):
-            msgBox = QtGui.QMessageBox(QtGui.QMessageBox.Critical,'错误',f'超出索引(文件最大行数为{len(df.index)-1})!')
-            msgBox.exec_()
+            msgBox = QMessageBox(QMessageBox.Critical,'错误',f'超出索引(文件最大行数为{len(df.index)-1})!')
+            msgBox.exec()
             self.ui.statusbar.showMessage(f'超出索引(文件最大行数为{len(df.index)-1})!')
         else:
             try:
-                k, b = np.polyfit(df.loc[min(co,ci):max(co,ci),'磁场(G)'], df.loc[min(co,ci):max(co,ci),'电流(A)'], 1)
+                k, _ = np.polyfit(df.loc[min(co,ci):max(co,ci),'磁场(G)'], df.loc[min(co,ci):max(co,ci),'电流(A)'], 1)
             except:
-                msgBox = QtGui.QMessageBox(QtGui.QMessageBox.Critical,'错误','所选区间无法线性拟合，请选择合适区间!')
-                msgBox.exec_()
+                msgBox = QMessageBox(QMessageBox.Critical,'错误','所选区间无法线性拟合，请选择合适区间!')
+                msgBox.exec()
                 self.ui.statusbar.showMessage('所选区间无法线性拟合，请选择合适区间!')
             else:
-                df['电流(A)'] = df['电流(A)']-k*df['磁场(G)']
-                self.curve.setData(df['磁场(G)'],df['电流(A)'])
-
+                df['电流(A)'] = (df['电流(A)']-k*df['磁场(G)'])
+                def fit_func(x,M,r,b,c):
+                    return M*np.tanh(r*x)-b*x-c
+                try:
+                    popt, pcov = curve_fit(fit_func, df['磁场(G)'],df['电流(A)'],p0=(df['电流(A)'].max(),0.002,0,(df['电流(A)'].max()+df['电流(A)'].min())/2),method='trf')
+                    df['电流(A)'] += popt[-1]
+                    self.ui.statusbar.showMessage(f'拟合法拉第测试结果为 {fit_func(df['磁场(G)'], *popt).max()+popt[-1]:g} deg')
+                except:
+                    self.ui.statusbar.showMessage(f'未能成功拟合测试数据')
+                finally:
+                    self.curve.setData(df['磁场(G)'],df['电流(A)'])
+                    
     def plot3(self):
         self.ui.qtplot.setLabel("left", "法拉第旋角 (deg/cm)")
         df = pd.read_excel(self.file[0],usecols=['磁场(G)','电流(A)'])
@@ -148,49 +148,55 @@ class Stats(TemplateBaseClass):
         co = self.ui.spinBox_start.value()
         ci = self.ui.spinBox_end.value()
         if co == ci:
-            msgBox = QtGui.QMessageBox(QtGui.QMessageBox.Critical,'错误','起始行数和结束行数必须不同!')
-            msgBox.exec_()
+            msgBox = QMessageBox(QMessageBox.Critical,'错误','起始行数和结束行数必须不同!')
+            msgBox.exec()
             self.ui.statusbar.showMessage('起始行数和结束行数必须不同!')
         elif co>len(df.index) or ci>len(df.index):
-            msgBox = QtGui.QMessageBox(QtGui.QMessageBox.Critical,'错误',f'超出索引(文件最大行数为{len(df.index)-1})!')
-            msgBox.exec_()
+            msgBox = QMessageBox(QMessageBox.Critical,'错误',f'超出索引(文件最大行数为{len(df.index)-1})!')
+            msgBox.exec()
             self.ui.statusbar.showMessage(f'超出索引(文件最大行数为{len(df.index)-1})!')
         else:
             try:
-                k, b = np.polyfit(df.loc[min(co,ci):max(co,ci),'磁场(G)'], df.loc[min(co,ci):max(co,ci),'电流(A)'], 1)
+                k, _ = np.polyfit(df.loc[min(co,ci):max(co,ci),'磁场(G)'], df.loc[min(co,ci):max(co,ci),'电流(A)'], 1)
             except:
-                msgBox = QtGui.QMessageBox(QtGui.QMessageBox.Critical,'错误','所选区间无法线性拟合，请选择合适区间!')
-                msgBox.exec_()
+                msgBox = QMessageBox(QMessageBox.Critical,'错误','所选区间无法线性拟合，请选择合适区间!')
+                msgBox.exec()
                 self.ui.statusbar.showMessage('所选区间无法线性拟合，请选择合适区间!')
             else:
                 df['电流(A)'] = (df['电流(A)']-k*df['磁场(G)'])/self.ui.thickness.value()*1e7    
-                self.curve.setData(df['磁场(G)'],df['电流(A)'])
+                def fit_func(x,M,r,b,c):
+                    return M*np.tanh(r*x)-b*x-c
+                try:
+                    popt, pcov = curve_fit(fit_func, df['磁场(G)'],df['电流(A)'],p0=(df['电流(A)'].max(),0.002,0,(df['电流(A)'].max()+df['电流(A)'].min())/2),method='trf')
+                    df['电流(A)'] += popt[-1]
+                    self.ui.statusbar.showMessage(f'拟合法拉第测试结果为 {fit_func(df['磁场(G)'], *popt).max()+popt[-1]:g} deg/cm')
+                except:
+                    self.ui.statusbar.showMessage(f'未能成功拟合测试数据')
+                finally:
+                    self.curve.setData(df['磁场(G)'],df['电流(A)'])
+                    
 
-
-
-QtGui.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
-QtGui.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
-pg.mkQApp()
-# WindowTemplate, TemplateBaseClass = pg.Qt.loadUiType("fr.ui")
-app = QtGui.QApplication([])
+app = QApplication([])
 translator = QtCore.QTranslator()
 translator.load("qt_zh_CN.qm")
 app.installTranslator(translator)
-app.setFont(QtGui.QFont('微软雅黑'))
+app.setFont(QFont('微软雅黑'))
+app.setWindowIcon(QIcon('logo.png'))
 
 splash_pix=QtGui.QPixmap('SplashScreen.png').scaled(600, 600, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
-splash = QtGui.QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
-# splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+splash = QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
 splash.show()
+time.sleep(1)
 
 stats = Stats()
-# mw=stats
-qtmodern.styles.dark(QtGui.QApplication.instance())
-mw = qtmodern.windows.ModernWindow(stats)
-mw.setWindowIcon(QtGui.QIcon('logo.png'))
-screen=app.primaryScreen().geometry()
-size=mw.geometry()
-mw.move((screen.width() - size.width()) // 2,(screen.height() - size.height()) // 3)
-mw.show()
-splash.finish(mw)
-app.exec_()
+stats.ui.setWindowIcon(QIcon('logo.png'))
+stats.ui.setStyleSheet("*{font-family:微软雅黑;font-size:14pt;}")
+screen = app.primaryScreen().geometry()
+size = stats.ui.geometry()
+stats.ui.resize(size.width(), size.width()*10/16+30)
+size = stats.ui.geometry()
+stats.ui.move((screen.width() - size.width()) / 2,
+              (screen.height() - size.height()) / 2)
+stats.ui.show()
+splash.finish(stats.ui)
+app.exec()
